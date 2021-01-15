20d1088ef1b93618d55381f02058511b54bca4dd
Elogeek <elodiechristin@gmail.com>
2021-01-15 19:20:02 +0100

    quizz

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = $(".question.json")
+let reponse = ["3", "3", "1", "1", "1", "2", "1", "1", "1", "1"];
+let score = 0;
+
+function stratQuizz() {
+    $('#paly').click(function ()
+    {
+
+    })
+}
